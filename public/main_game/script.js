ad9c67e91a4ebb5d93f8525c227ad84d8138ac43
Heather Stone <heather.blair.stone@gmail.com>
2016-05-17 11:24:13 -0600

    add api call and scroll image positioning

--- public/main_game/script.js
+++ public/main_game/script.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
+var request = new XMLHttpRequest();
+request.onreadystatechange = function(){
+ if (request.readyState == 4 && request.status == 200) {
+var parse =   JSON.parse(request.responseText);
+   console.log(parse);
+     var quote =(parse.contents.quotes[0].quote);
+ $(".scroll").append(quote);
+     console.log(quote);
+ JSON.parse(request.responseText);
+ }
+};
 
-})
+request.open("GET", "http://quotes.rest/qod.json");
+
+request.send();
