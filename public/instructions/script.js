a50f1db737e628c146104a4735fcd46b51f53130
Heather Stone <heather.blair.stone@gmail.com>
2016-05-17 14:56:23 -0600

    fix background, add sunflowers, add spacebar-to-url change function

--- public/instructions/script.js
+++ public/instructions/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   $(window).keypress(function(e) {
     if (e.keyCode == 0 || e.keyCode == 32) {
-      console.log('Space pressed');
+      console.log('iagooooo!');
+      window.location = "/main_game/index.html";
+      // $(document).location.pathName = "/main_game/index.html"
     }
   });
-})
+});
---
+++ public/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(window).keypress(function(e) {
+    if (e.keyCode == 0 || e.keyCode == 32) {
+      console.log('iagooooo');
+    }
+  });
+})
